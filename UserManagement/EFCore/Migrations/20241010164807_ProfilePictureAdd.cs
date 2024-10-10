1601aa77a170a6913133b29399a9f02a09bcd13d
seif amr <seifamr410@gmail.com>
2024-10-10 19:49:26 +0300

    Migrations Add

---
+++ UserManagement/EFCore/Migrations/20241010164807_ProfilePictureAdd.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProfilePictureAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfilePicture",
+                table: "AspNetUsers",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                table: "AspNetUsers");
+        }
+    }
+}
